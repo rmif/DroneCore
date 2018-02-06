cb806fe0afc0fc36b2169f0a17e9f6aaac5692da
Jonas Vautherin <jonas.vautherin@gmail.com>
2018-02-06 01:15:38 +0100

    blah

--- grpc/server/cmake/helpers/build_target.cmake
+++ grpc/server/cmake/helpers/build_target.cmake
@@ -5,6 +5,10 @@ function(build_target SRC_DIR TARGET_DIR)
         COMMAND ${CMAKE_COMMAND}
             -G${CMAKE_GENERATOR}
             -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+            -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
+            -DBINARY_DIR=${TARGET_DIR}
+            -DANDROID_ABI=${ANDROID_ABI}
+            -DANDROID_STL=${ANDROID_STL}
             ${SRC_DIR}
         WORKING_DIRECTORY ${TARGET_DIR}/build
     )
